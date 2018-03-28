9df5f4e9864f9ddae176e8c9dc10089c51f2875b
MajaDurkovic <maja.dj000@gmail.com>
2018-03-28 22:00:41 +0200

    Druga verzija app

--- app/src/main/java/hr/ferit/maja/rma_zadaca/MainActivity.java
+++ app/src/main/java/hr/ferit/maja/rma_zadaca/MainActivity.java
@@ -1,13 +1,61 @@
 package hr.ferit.maja.rma_zadaca;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton billgates = findViewById(R.id.Picture1);
+
+        billgates.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast notification = Toast.makeText(MainActivity.this, "\"If you can't make it good, at least make it look good.\" -Bill Gates", Toast.LENGTH_LONG);
+                notification.show();
+            }
+        }));
+
+        ImageButton elonmusk = findViewById(R.id.Picture2);
+
+        elonmusk.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast notification = Toast.makeText(MainActivity.this, "\"I could either watch it happen or be a part of it.\" - Elon Musk", Toast.LENGTH_LONG );
+                notification.show();
+            }
+        }));
+
+        ImageButton markzuckerberg = findViewById(R.id.Picture3);
+
+        markzuckerberg.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast notification = Toast.makeText(MainActivity.this, "\"Unless you are breaking stuff, you are not moving fast enough.\"-Mark Zuckerberg", Toast.LENGTH_LONG );
+                notification.show();
+            }
+        }));
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
---
+++ app/src/main/java/hr/ferit/maja/rma_zadaca/OnClick.java
@@ -0,0 +1,8 @@
+package hr.ferit.maja.rma_zadaca;
+
+/**
+ * Created by Maja on 3/28/2018.
+ */
+
+@interface OnClick {
+}
